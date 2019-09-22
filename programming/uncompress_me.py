b55536d4572d048b0a22932bb447d6bd69e6fad9
root <root@localhost.localdomain>
2019-09-22 16:59:26 -0400

    Added four first programming challenges

---
+++ programming/uncompress_me.py
@@ -0,0 +1,57 @@
+import socket, string, time, thread, math, base64
+
+SERVER = 'irc.root-me.org'
+PORT = 6667
+NICKNAME = 'sptoom'
+CHANNEL = '#root-me_challenge'
+BOTNAME = 'Candy'
+
+def irc_connect():
+    global IRC
+    IRC = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    IRC.connect((SERVER, PORT))
+
+def irc_command(command):
+    print(command)
+    IRC.send(command + '\n')
+
+irc_connect()
+
+irc_command("USER %s %s %s : Hey" % (NICKNAME, NICKNAME, NICKNAME))
+irc_command("NICK %s" % NICKNAME)
+irc_command("JOIN %s" % CHANNEL)
+
+while (1):
+    text = IRC.recv(1024)
+    print(text)
+	
+    if text.find('PING') != -1:
+        # answer PING from IRC server
+        msg = text.split()
+        if msg[0] == "PING":
+	    irc_command("PONG %s" % msg[1] + '\n')
+    elif text.find("%s +x" % NICKNAME) != -1:
+        # detect that IRC chat was fully initialized and send PRIVMSG to bot
+        irc_command("PRIVMSG %s : !ep4" % BOTNAME)
+    elif text.find(BOTNAME) != -1 and text.find("PRIVMSG %s" % NICKNAME) != -1 and text.find("Bad") == -1 and text.find("password") == -1:
+        # detect and parse bot answer
+        rot13string = text.split(':')[2].split('\r')[0]
+        print(rot13string)
+        
+        result = ""
+        for c in rot13string:
+            if c <= '9':
+                rot13 = ord(c)
+            elif c >= 'a':
+                rot13 = ord(c) + 13
+                rot13 -= ord('a')
+                rot13 %= 26
+                rot13 += ord('a')
+            else:
+                rot13 = ord(c) + 13
+                rot13 -= ord('A')
+                rot13 %= 26
+                rot13 += ord('A')
+            result += chr(rot13)
+        
+        irc_command("PRIVMSG %s : !ep4 -rep %s" % (BOTNAME, result))
